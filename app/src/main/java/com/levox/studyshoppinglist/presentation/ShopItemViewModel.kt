11f381412f29b71c3b240b93a8982110506c58cd
Levox98 <ivanov.ks101@gmail.com>
2022-08-21 12:18:52 +0500

    Created view model for the second screen

---
+++ app/src/main/java/com/levox/studyshoppinglist/presentation/ShopItemViewModel.kt
@@ -0,0 +1,64 @@
+package com.levox.studyshoppinglist.presentation
+
+import androidx.lifecycle.ViewModel
+import com.levox.studyshoppinglist.data.ShopItemRepositoryImpl
+import com.levox.studyshoppinglist.domain.*
+
+class ShopItemViewModel : ViewModel() {
+
+    private val repository = ShopItemRepositoryImpl
+
+    private val getShopItemUseCase = GetShopItemUseCase(repository)
+    private val addShopItemUseCase = AddShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    var id: Int? = null
+    val shopItem = repository.getShopItem(id!!)
+
+    fun getShopItem(shopItemId: Int) {
+        val item = getShopItemUseCase.getShopItem(shopItemId)
+    }
+
+    fun addShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        val inputValidated = isInputValid(name, count)
+        if (inputValidated) {
+            val shopItem = ShopItem(name, count, true)
+            addShopItemUseCase.addShopItem(shopItem)
+        }
+    }
+
+    fun editShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        val inputValidated = isInputValid(name, count)
+        if (inputValidated) {
+            val shopItem = ShopItem(name, count, true)
+            editShopItemUseCase.editShopItem(shopItem)
+        }
+    }
+
+    private fun parseName(inputName: String?): String {
+        return inputName?.trim() ?: ""
+    }
+
+    private fun parseCount(inputCount: String?): Int {
+        return try {
+            inputCount?.trim()?.toInt() ?: 0
+        } catch (e: Exception) {
+            0
+        }
+    }
+
+    private fun isInputValid(name: String, count: Int): Boolean {
+        var result = true
+        if (name.isBlank()) {
+            result = false
+        }
+        if (count <= 0) {
+            result = false
+        }
+        return result
+    }
+}
