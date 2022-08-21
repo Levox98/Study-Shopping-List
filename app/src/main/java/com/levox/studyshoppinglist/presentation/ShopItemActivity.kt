9c1c6c056ad69404045229c7aef21e7e2f478853
Levox98 <ivanov.ks101@gmail.com>
2022-08-21 11:49:53 +0500

    Created second screen

---
+++ app/src/main/java/com/levox/studyshoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.levox.studyshoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.levox.studyshoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
---
+++ app/src/main/java/com/levox/studyshoppinglist/presentation/ShopItemViewHolder.kt
@@ -0,0 +1,11 @@
+package com.levox.studyshoppinglist.presentation
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.levox.studyshoppinglist.R
+
+class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    val tvName: TextView = view.findViewById(R.id.tv_name)
+    val tvCount: TextView = view.findViewById(R.id.tv_count)
+}
